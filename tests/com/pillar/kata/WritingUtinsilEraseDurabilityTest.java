ecb7c550264753bb07c1ce8ee3ccf043792765d0
mtedder <maurice_tedder@hotmail.com>
2019-08-29 00:57:32 -0400

    Created testPencilEraseDurabilityBreakEvenLowerCase which passed green on initial test.

---
+++ tests/com/pillar/kata/WritingUtinsilEraseDurabilityTest.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package com.pillar.kata;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.pillar.kata.impl.Paper;
+import com.pillar.kata.impl.Pencil;
+import com.pillar.kata.interfaces.Media;
+import com.pillar.kata.interfaces.WritingUtinsil;
+
+/**
+ * @author Maurice Tedder
+ *
+ */
+class WritingUtinsilEraseDurabilityTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testPencilEraseDurabilityBreakEvenLowerCase() {
+		
+		String expected = "    ";		
+		
+		Media paper = new Paper();
+		WritingUtinsil pencil = new Pencil();
+		
+		paper = pencil.write(paper, "test");//add content
+		
+		paper = pencil.erase(paper, "test");	
+		String actual = paper.getContent();
+				
+		assertEquals(expected, actual, "testPencilEraseDurabilityBreakEvenLowerCase Failed");
+	}
+
+}
